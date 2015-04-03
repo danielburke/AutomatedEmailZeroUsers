6d3344991a75a78dc9e177bf11c7fa74ae71b62e
Daniel Burke <daniel.burke397@gmail.com>
2015-04-03 13:31:27 -0500

    Update AUtomatedEmailZeroUsers.ps1

--- AUtomatedEmailZeroUsers.ps1
+++ AUtomatedEmailZeroUsers.ps1
@@ -1,92 +1,56 @@
-$owner=@()
-$DL=@()
-$EmailAddress=@()
-$AllowedSenders=@()
-$ownerAllowed=@()
-$DLAllowed=@()
-$EmailAddressAllowed=@()
-$z=0
-$y=0
-$Outlook= New-Object -ComObject Outlook.Application
-$Mail=$outlook.CreateItem(0)
-$listOfDls=""
-$Mail.Subject="DG MAINT Owner Action Required"
+$dgmaint_analysis = Import-csv 'C:\Users\DBURK1\Desktop\LDAP Cleanup\dgmaint-analysis.csv'
+[array]$dlnames = $null
+[array]$dlowners = $null
+[array]$dlallowedsenders = $null
+[array]$unique_owners_senders = $null
+[array]$owner=$null
+[array]$sender=$null
+[array]$inactive=$null
+$mail=new-object Net.Mail.MailMessage
+$smtp=new-object Net.Mail.SmtpClient("mail.monsanto.com")
 
+$i=0
+foreach ($dl in $dgmaint_analysis) {
+    $dlnames += $dl.Group_Name
+    $unique_owners_senders += $dl.Group_Owner
 
-#Loads all columns into arrays
-Import-csv 'C:\Users\DBURK1\Desktop\LDAP Cleanup\Zero Result.csv' | ForEach-Object{
-    $owner+=$_.owner
-    $DL+=$_.dlname
-    $results=& 'C:\Users\DBURK1\Desktop\LDAP Cleanup\IsActiveIDM-NonUser.ps1' $._emailaddress
-    if($results -eq 'Active'){
-        $EmailAddress+=$_.emailaddress
-    
-    $x++
-}
-Import-csv 'C:\Users\DBURK1\Desktop\LDAP Cleanup\Zero ResultAllowedSenders.csv' | ForEach-Object{
-    $ownerAllowed+=$_.owner
-    $DLAllowed+=$_.dlname
-    $results=& 'C:\Users\DBURK1\Desktop\LDAP Cleanup\IsActiveIDM-NonUser.ps1' $._emailaddress
-    if($results -eq 'Active'){
-        $EmailAddressAllowed+=$_.emailaddress
+    if($dl.Allowed_Senders -ne "") {               
+                    [array]$dlallowedsender_entry = $dl.Allowed_Senders.Split('|')
+                    $unique_owners_senders += $dlallowedsender_entry
     }
-    $AllowedSenders+=$_.allowedsenders
-    $x++
-}
-echo $
-#Emails the owner and allowed sender
-while($y -le $dl.count){
-    #construct an email object
-    #Find out if the next DL on the list is owned by the same person
-    #if so, append the DL, else, send email
-
-
-    $Mail.To="Daniel.1.Burke@monsanto.com" #$EmailAddress[$y] 
-    $listOfDls+= $DL[$y]
-    while($owner[$y] -eq $owner[$y+1] -and $y -lt $dl.Count){
-        $listOfDls+=$DL[$y+1]+" "
-        $y++
-    } 
-    $Mail.Body=('Hello <NAME>,
-
-I am working with the Enterprise Identity Solutions team to help automated DLs become more efficient and more precise. Part of this task is to look at all of the current automated DLs and find a better way to return the same results, while doing this I have found that you are the owner of' +$listOfDls+'
-
-Currently, this DL has zero users and is not returning any results when we process the query. 
-
-Is'+$listOfDls + 'still valid, if so please let me know, as well as the what the goal or intended purpose of this DL is so I can help you remediate the issue. If it is not valid please respond as such and we will delete it for you.
-
-Please note, we will delete said DL if no response is received by <DATE>
-
-')
-    $y++
-    echo $listOfDls
-    $listOfDls=""
 }
 
-while($z -le $DLAllowed.count){
-    #construct an email object
-    #Find out if the next DL on the list is owned by the same person
-    #if so, append the DL, else, send email
-
-
-    $Mail.To="Daniel.1.Burke@monsanto.com" #$EmailAddress[$y] 
-    $listOfDls+= $DLAllowed[$z]
-    while($owner[$z] -eq $owner[$z+1] -and $z -lt $DLAllowed.Count){
-        $listOfDls+=$DL[$z+1]+" "
-        $z++
-    } 
-    $Mail.Body=('Hello <NAME>,
-
-I am working with the Enterprise Identity Solutions team to help automated DLs become more efficient and more precise. Part of this task is to look at all of the current automated DLs and find a better way to return the same results, while doing this I have found that you are the owner of' +$listOfDls+'
-
-Currently, this DL has zero users and is not returning any results when we process the query. 
-
-Is'+$listOfDls + 'still valid, if so please let me know, as well as the what the goal or intended purpose of this DL is so I can help you remediate the issue. If it is not valid please respond as such and we will delete it for you.
-
-Please note, we will delete said DL if no response is received by <DATE>
-
-')
-    $z++
-    echo $listOfDls
-    $listOfDls=""
+$unique_owners_senders = $unique_owners_senders | Select-Object -Unique
+echo $unique_owners_senders
+foreach($user in $unique_owners_senders){
+    [array]$owner=$null
+    [array]$sender=$null
+    [array]$inactive=$null
+    foreach($dl in $dgmaint_analysis){
+       
+        if($dl.Group_Owner-eq $unique_owners_senders[$i]){
+            $owner+=$dl.Group_Name
+        }
+        [array]$AllowedSenders_atLine=$dl.Allowed_Senders.Split('|')
+        if($AllowedSenders_atLine-contains $unique_owners_senders[$i]){
+            $sender+=$dl.Group_Name
+        }
+        if($dl.'Number of Group Members' -eq 0 -and $dl.Group_Owner -eq $users ){
+            $inactive+=$dl.Group_Name
+        }
+    $mail.From-"Daniel.1.burke@monsanto.com"
+    $mail.To.add($unique_owners_senders[$i]+"@monsanto.com")
+    $mail.Subject="DGMAINT"
+    $mail.Body=get-Content 'C:\Users\DBURK1\Desktop\LDAP Cleanup\DG MAINT Warning.htm' -f $dl.Group_Owner, $owner, $sender
+    if($inactive.count -ne 0){
+        $Mail2=new-object Net.Mail.MailMessage
+        $Mail2.To.add($unique_owners_senders[$i]+"@monsanto.com")
+        $Mail2.Subject="DG MAINT ACTION REQUIRED"
+        $Mai2.Body=get-Content 'C:\Users\DBURK1\Desktop\LDAP Cleanup\DG MAINT.htm' -f $dl.Group_Owner, $owner, $inactive
+    
+    }
+    $i++
+    }
+# $smtp.Send($mail)
+# $smtp.Send($Mail2)
 }
